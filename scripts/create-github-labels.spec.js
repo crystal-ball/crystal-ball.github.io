d7555216042902048254085759a72763411619b7
Dan Hedgecock <hedgecock.d@gmail.com>
2019-09-05 08:14:41 -0700

    New: Add Jest testing setup to project 💖

---
+++ scripts/create-github-labels.spec.js
@@ -0,0 +1,7 @@
+'use strict'
+
+// ℹ️ unit test by mocking node-fetch and inquirer
+describe('createGithubLabels', () => {
+  test.todo('script should request existing labels')
+  test.todo('script should create labels')
+})
