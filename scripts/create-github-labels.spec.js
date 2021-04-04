b07a924df3b01e16c08aca2c709c3016bab1a006
Dan Hedgecock <hedgecock.d@gmail.com>
2021-04-04 09:57:56 -0700

    Upgrade: Project dependencies 🆙

--- scripts/create-github-labels.spec.js
+++ scripts/create-github-labels.spec.js
@@ -1,5 +1,6 @@
 // ℹ️ unit test by mocking node-fetch and inquirer
 describe('createGithubLabels', () => {
-  test.todo('script should request existing labels')
-  test.todo('script should create labels')
+  it.todo('script should request existing labels')
+
+  it.todo('script should create labels')
 })
